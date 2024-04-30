e2d7e4e83912bd8a7c92eb47ab107e66f8bb9525
P.J. Bleau <pj@simplebet.ai>
2024-04-30 15:27:15 -0400

    additions

--- .custom_commands_bleaup.sh
+++ .custom_commands_bleaup.sh
@@ -71,3 +71,32 @@ function gch() {
 function gf() {
   git fetch
 }
+
+function diff() {
+    git log --pretty=format:"%h - %an: %s" v0.1.$1..v0.1.$2
+}
+
+function pj_diff() {
+  git log --pretty=format:"%Cgreen%<(135,trunc)%s%n%Cblue    >> %<(18,trunc)%an | %as, %Cred %h [%<(10,trunc)%(describe:tags=true)]%n" v0.1.$1..v0.1.$2
+}
+
+function py_run() {
+  if [ $1 = "main" ]
+  then
+    python schedule_builder.py
+  elif [ $1 = "test" ]
+  then
+    python schedule_builder_test.py
+  elif [ $1 = "formatter" ]
+  then
+    python main_PJ_unified.py
+  elif [ $1 = "Kate" ]
+  then
+    echo "Mom"
+  elif [ $1 = "Francisco" ]
+  then
+    echo "Dad"
+  else
+    echo "Invalid File Name"
+  fi
+}
